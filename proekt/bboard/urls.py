15ebbc36c4acdbb831fb0bf3512f2a8a42a2ba5f
qwerty2281444 <alihanryspek044@gmail.com>
2025-06-14 18:40:39 +0500

    commit by alihan 3

--- proekt/bboard/urls.py
+++ proekt/bboard/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from views import FirstView
+from views import FirstView,SecondView,ThirdView
 
 urlpatterns = [
-    path('', FirstView.as_view(), name='one')
+    path('', FirstView.as_view(), name='one'),
+    path('two/', SecondView.as_view(), name='two'),
+    path('three/', FirstView.as_view(), name='three')
 ]
--- proekt/bboard/views.py
+++ proekt/bboard/views.py
@@ -4,3 +4,9 @@ from django.views.generic import TemplateView
 
 class FirstView(TemplateView):
     template_name = 'first.html'
+
+class SecondView(TemplateView):
+    template_name = 'second.html'
+
+class ThirdView(TemplateView):
+    template_name = 'third.html'
