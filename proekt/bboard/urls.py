44a9a0d4a454a7a7d23afbfe45d024a06aaf53cd
qwerty2281444 <alihanryspek044@gmail.com>
2025-06-14 18:44:04 +0500

    last commit by alihan maybe?

--- proekt/bboard/urls.py
+++ proekt/bboard/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from views import FirstView,SecondView,ThirdView
+from .views import FirstView,SecondView,ThirdView
 
 urlpatterns = [
     path('', FirstView.as_view(), name='one'),
     path('two/', SecondView.as_view(), name='two'),
-    path('three/', FirstView.as_view(), name='three')
+    path('three/', ThirdView.as_view(), name='three')
 ]
